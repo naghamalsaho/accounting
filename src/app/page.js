45eb4d12863e664612ae37e7f64df6643dd581d7
nn@gmail.com <nn@gmail.com>
2026-09-01 23:18:22 +0000

    first commit2345

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,16 @@
 'use client';
 
+import { useEffect } from 'react';
 import AppRouter from './router';
 
 export default function Home() {
+  useEffect(() => {
+    // التحقق من أن الكود يعمل في المتصفح
+    if (typeof window !== 'undefined') {
+      // يمكنك استخدام document هنا بأمان[cite: 13]
+      console.log(document.title);
+    }
+  }, []);
+
   return <AppRouter />;
 }
